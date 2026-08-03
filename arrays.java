ef47e095056104bd7d47aea3d935ae1e64bea8e8
Tanya-26 <tamvimaheshwari26@gmail.com>
2026-08-03 16:32:26 +0530

    Arrays

---
+++ arrays.java
@@ -0,0 +1,6 @@
+// 1st data structure
+// same typr of data
+// contiguous in memory + linear
+public class arrays {
+    
+}
--- hs.java
+++ hs.java
@@ -4,6 +4,7 @@ public class hs {
     public static void main(String[] args) {
         HashMap<Integer, Integer> hp = new HashMap<>();
         HashSet<Integer> hs = new HashSet<>();
-        
+        hp.put(1, 40);
+        hs.add(90);
     }
 }
