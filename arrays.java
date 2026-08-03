cc5b609d382db8952452f42c8dd3f42c6849c34b
Tanya-26 <tamvimaheshwari26@gmail.com>
2026-08-03 17:20:47 +0530

    Arrays

--- arrays.java
+++ arrays.java
@@ -1,6 +1,16 @@
 // 1st data structure
 // same typr of data
 // contiguous in memory + linear
-public class arrays {
-    
+public class arrays { 
+    // 1. This is the class body (Only declarations allowed here)
+
+    public static void main(String[] args) { 
+        // 2. This is the method body (Executable code goes here)
+        int marks[] = {90, 100, 89, 23, 45}; 
+
+        for(int i = 0; i < marks.length; i++){ 
+            System.out.println(marks[i]); 
+        } 
+    } 
 }
+
